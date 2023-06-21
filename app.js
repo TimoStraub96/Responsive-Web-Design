d9467c10771d8545a66e1c9f87fe3cd0cb81ddaa
TimoStraub96 <92566971+TimoStraub96@users.noreply.github.com>
2023-06-21 12:03:44 +0200

    Bilder Slideshow hinzugefügt

--- app.js
+++ app.js
@@ -9,3 +9,26 @@ menu.addEventListener('click', function(){
 });
 
 
+// JavaScript
+document.addEventListener("DOMContentLoaded", function() {
+  const viewports = document.querySelectorAll(".viewport");
+  let currentViewportIndex = 0;
+
+  function scrollToViewport(index) {
+    viewports[index].scrollIntoView({ behavior: "smooth" });
+  }
+
+  function handleScroll(event) {
+    const direction = event.deltaY > 0 ? "down" : "up";
+
+    if (direction === "down" && currentViewportIndex < viewports.length - 1) {
+      currentViewportIndex++;
+      scrollToViewport(currentViewportIndex);
+    } else if (direction === "up" && currentViewportIndex > 0) {
+      currentViewportIndex--;
+      scrollToViewport(currentViewportIndex);
+    }
+  }
+
+  document.addEventListener("wheel", handleScroll);
+});
